dae5db7d9ad96800d543e1f764c5a3fa7ed768d6
Tristan Schonfeldt-Aultman <115110616+TristanS-A@users.noreply.github.com>
2024-09-17 14:32:25 -0400

    Finished xor shift.

--- docs/artificialintelligence/assignments/rng/rng.cpp
+++ docs/artificialintelligence/assignments/rng/rng.cpp
@@ -1,16 +1,27 @@
 // add your imports here
+#include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <istream>
+
 const std::string TEST_FOLDER = "\\tests\\";
-unsigned int xorShift(unsigned int seed, int r1, int r2);
+uint32_t xorShift(uint32_t seed, int r1, int r2);
 int main(){
-  // code here
-  unsigned int seed, N, min, max;
+  uint32_t seed, N, min, max, result;
   std::cin >> seed >> N >> min >> max;
+
+  for (int i = 0; i < N; i++) {
+    seed = xorShift(seed, min, max);
+    std::cout << min + (seed % (max - min + 1)) << std::endl;
+  }
 }
 //The purpose of this function is to take the number and xor shift it to output a pseudo-random number
-    unsigned int xorShift(unsigned int seed, int r1, int r2)
+    uint32_t xorShift(uint32_t seed, int r1, int r2)
 {
+    uint32_t x = seed;
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
 
+    return x;
 }
